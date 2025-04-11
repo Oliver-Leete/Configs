759d59e03508f4fef07329634063b9a7d7e5b2d2
Oliver-Leete <oliverleete@gmail.com>
2025-04-11 15:28:22 +0100

    Added treesj with mini.splitjoin as fallback (requires mods to treesj to work)

---
+++ nvim/lua/plugins/coding/split_join.lua
@@ -0,0 +1,58 @@
+---@module "lazy"
+---@type LazySpec
+return {
+    {
+        "Wansmer/treesj",
+        dependencies = { "echasnovski/mini.splitjoin" },
+        opts = {
+            use_default_keymaps = false,
+            notify = false,
+            on_error = function(err_text, level, ...)
+                local msg = require("treesj.notify").msg
+                if err_text and not (
+                        err_text == msg.no_ts_parser
+                        or err_text == msg.no_configured_lang
+                        or err_text == msg.no_configured_node
+                        or err_text == msg.no_format_with
+                    )
+                then
+                    level = level or vim.log.levels.INFO
+                    vim.notify(err_text:format(...), level, { title = "TreeSJ: split/join code block " })
+                else
+                    require("mini.splitjoin").toggle()
+                end
+            end,
+        },
+        keys = {
+            {
+                ",j",
+                function() require("treesj").toggle() end,
+                desc = "Split/join",
+                mode = { "n", "x" }
+            }
+        },
+    },
+    {
+        "echasnovski/mini.splitjoin",
+        opts = {
+            mappings = {
+                toggle = "",
+            },
+            detect = {
+                separator = "[,;]",
+            },
+            split = {
+                hooks_pre = {},
+                hooks_post = {},
+            },
+            -- Join options
+            join = {
+                hooks_pre = {},
+                hooks_post = {},
+            },
+        },
+        keys = {
+            { ",J", function() require("mini.splitjoin").toggle() end, desc = "Split/join (force mini)", mode = { "n", "x" } }
+        }
+    }
+}
